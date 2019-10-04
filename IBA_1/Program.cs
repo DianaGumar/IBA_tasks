63ae80e94115570118434e19837d3c5c11a5c735
DianaGumar <lantan.mp4@gmail.com>
2019-10-04 18:01:39 +0300

    add vimpire numbers class and unit tests  for class methods

--- IBA_1/Program.cs
+++ IBA_1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using IBA_1.TasksClases;
 
 namespace IBA_1
 {
@@ -10,6 +11,16 @@ namespace IBA_1
     {
         static void Main(string[] args)
         {
+            //1 vampire number
+
+            List<int> vn = VampireNumber.GetExistVampireNumber(4);
+
+            for(int i = 0; i < vn.Count; i+=3)
+            {
+                Console.Write(vn.ElementAt(i) + " = " + vn.ElementAt(i+1) + " * " + vn.ElementAt(i+2) + "\n");
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IBA_1.TasksClases;
+using System.Collections.Generic;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class VampireNumber_UnitTest
+    {
+        [TestMethod]
+        public void GetNumberParts_Test()
+        {
+            int[] actual = VampireNumber.GetNumberParts(654321);
+            int[] expected = new[] { 1, 2, 3, 4, 5, 6 };
+
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void GetNumberTwoVariants_Test()
+        {
+            int[] i = VampireNumber.GetNumberParts(4321);
+
+            List<int> actual = VampireNumber.GetNumberTwoVariants(i);
+            List<int> expected = new List<int>() { 12, 13, 14, 21, 23, 24, 31, 32, 34, 41, 42, 43 };
+
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void IsRealMakeVimpireNumber_Test()
+        {
+            int num = 1827;
+            Boolean expected = true;
+
+            int[] i = VampireNumber.GetNumberParts(num);
+            List<int> ii = VampireNumber.GetNumberTwoVariants(i);
+
+            Boolean actual = VampireNumber.IsRealMakeVimpireNumber(ii, num);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void GetExistVampireNumber_Test()
+        {
+            List<int> actual = VampireNumber.GetExistVampireNumber(4);
+
+            List<int> expected = new List<int>() { };
+
+            CollectionAssert.AreEqual(expected, actual);
+
+        }
+
+    }
+}
